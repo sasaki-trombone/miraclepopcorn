87b7b69e530cdcbbde40072f240faf567106d9d3
Kosuke Sasaki <27848990+sasaki-trombone@users.noreply.github.com>
2024-09-02 00:20:13 +0900

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.26.0
      project name = miraclepopcorn
      framework = remix
      framework cli = create-remix@2.11.1
      package manager = npm@10.8.2
      wrangler = wrangler@3.73.0
      git = 2.46.0

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,37 +1,20 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/cloudflare";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    {
+      name: "description",
+      content: "Welcome to Remix on Cloudflare!",
+    },
   ];
 };
 
 export default function Index() {
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
+      <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
       <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
         <li>
           <a
             className="text-blue-700 underline visited:text-purple-900"
@@ -40,6 +23,16 @@ export default function Index() {
             rel="noreferrer"
           >
             Remix Docs
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
+            rel="noreferrer"
+          >
+            Cloudflare Pages Docs - Remix guide
           </a>
         </li>
       </ul>
